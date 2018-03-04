408d3900aa58a412935fc4a1d0ad03ebf342ad24
Trishul <trishul.goel@gmail.com>
2018-03-04 18:37:22 +0530

    fs promise implementation

--- create-manifest.js
+++ create-manifest.js
@@ -1,12 +1,14 @@
-
-const fs = require('fs');
+const fs = require('fs-extra')
 const path = require('path');
+const jsonFormat = require('json-format');
 
 const createManifest = function (payload) {
-  fs.mkdir(path.join(__dirname, payload.name), function(err, dir) {
-    const manifestStream = fs.createWriteStream(path.join(__dirname, payload.name, 'manifest.json'));
-    manifestStream.write(JSON.stringify(payload));
-  })
+	fs.ensureDir(path.join(__dirname, payload.name))
+		.then((dir) => {
+			fs.outputFile(`${dir}/manifes.json`, jsonFormat(payload))
+				.catch(err => { throw err });
+		})
+		.catch(err => { throw(err) });
 };
 
 module.exports = createManifest;
