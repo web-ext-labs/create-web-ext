96f5d03cbf53da30ecdd5e80d646cb0695b3a278
Trishul <trishul.goel@gmail.com>
2018-03-04 23:06:38 +0530

    packaging and read me

--- index.js
+++ index.js
@@ -1,58 +1,16 @@
 #!/usr/bin/env node
 
-const inquirer = require('inquirer');
-const fs = require('fs');
-const createManifest = require('./create-manifest');
+const startHere = require('./src/start');
+const pJson = require('./package.json');
+const args = process.argv;
 
-inquirer
-    .prompt([{
-            type: 'input',
-            name: 'name',
-            message: `What's the name of your add-on?`,
-            default: 'My extension'
-        },
-        {
-            type: 'input',
-            name: 'description',
-            message: 'Description of the add-on?'
-        },
-        {
-            type: 'input',
-            name: 'version',
-            message: 'Version of your add-on?',
-            default: '1.1.1'
-        },
-        {
-            type: 'confirm',
-            name: 'background_script',
-            message: 'Would you like to add a background script?',
-            default: false
-        },
-        {
-            type: 'confirm',
-            name: 'content_script',
-            message: 'Would you like to add a content script?',
-            default: false
-        },
-        {
-            type: 'confirm',
-            name: 'browser_action',
-            message: 'Would you like to add a browser action?',
-            default: false
-        },
-        {
-            type: 'confirm',
-            name: 'page_action',
-            message: 'Would you like to add a page action?',
-            default: false
-        },
-        {
-            type: 'confirm',
-            name: 'options_ui',
-            message: 'Would you like to add a extensions option?',
-            default: false
-        }
-    ])
-    .then(answers => {
-        createManifest(answers);
-    });
+if(args.length > 2) {
+    if(args[2] === '-v') {
+        console.log(`create-web-ext version ${pJson.version}`);
+        return true;
+    } else {
+        console.log(`create-web-ext do not require any argument`);
+    }
+}
+
+startHere();
